1405f8ff96206f188edf3f77f2fa0d2addb8f0b4
Kaarel Pehap <kaarel.pehap@voco.ee>
2025-01-29 11:34:48 +0200

    m

--- apache_paigaldus.sh
+++ apache_paigaldus.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# kontrollime, kas apache2 on juba paigaldatud
+
 status=$(dpkg-query -W -f='${status}' apache2 | grep -c 'ok installed')
 
-if [ "$status" -eq 0 ]; then
+if [ $status -eq 0 ]; then
   echo "Apache2 ei ole paigaldatud, paigaldame apache2"
   apt install apache2
 elif [ $status -eq 1 ]; then
   echo "Apache2 on paigaldatud"
-  service apache2 start
-  service apache2 status
 fi
+systemctl start apache2
+systemctl status apache2
